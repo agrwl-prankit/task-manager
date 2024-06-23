53ba40456b80f7cec2489f49d041e23afcbc535e
agrwl-prankit <agrwnlprankit77@gmail.com>
2024-06-23 15:11:27 +0530

    create serializer and viewset for crud in backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -125,6 +126,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000'
-)
+CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000']
